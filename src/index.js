afa7895c35bfd661fa3539cd28a60bdfcb302f95
basilgoodluck <basilgoodluck22@gmail.com>
2024-11-20 21:06:43 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -17,28 +17,29 @@ const hostname = process.env.HOSTNAME || 'localhost';
 const server = http.createServer((req, res) => {
     const { url, method } = req;
 
+    console.log(`Incoming request: ${method} ${url}`);
+
     res.setHeader("Content-Type", "application/json");
 
-    if (url === "/" && method === "GET") {
+    const path = url.split('?')[0];
+
+    if (path === "/" && method === "GET") {
         res.statusCode = 200;
         res.end(JSON.stringify({ message: "Welcome to my telegram bot" }));
-    } else if (url === "/api/data" && method === "GET") {
+    } 
+    else if (path === "/api/data" && method === "GET") {
         res.statusCode = 200;
         res.end(JSON.stringify({ message: "This is the about page" }));
-    } else {
+    } 
+    else {
         res.statusCode = 404;
         res.end(JSON.stringify({ message: "No route found" }));
     }
 });
 
-server.listen(PORT, hostname, () => {
+server.listen(PORT, () => {
     console.log(`Server is running at http://${hostname}:${PORT}`);
 });
-
-
-async function createInviteLink() {
-    return `https://t.me/noble_la_bot?startgroup=admin&admin=delete_messages+manage_chat+restrict_members+pin_messages`;
-}
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -47,39 +48,39 @@ const lock_file = path.join(__dirname, "bot.lock")
 
 const startBot = async () => {
     try {
-        if(fs.existsSync(lock_file)){
-            console.log("Bot is already running! If not, delete bot.lock file.")
-            process.exit(1)
+        if (fs.existsSync(lock_file)) {
+            console.log("Bot is already running! If not, delete bot.lock file.");
+            process.exit(1);
         }
 
-        fs.writeFileSync(lock_file, "locked")
-        await bot.launch()
-        console.log("Bot has started successfully")
+        fs.writeFileSync(lock_file, "locked");
+        await bot.launch();
+        console.log("Bot has started successfully");
 
         process.once("SIGINT", () => {
-            bot.stop("SIGINT")
-            console.log("Bot stopped SIGINT")
-        })
+            bot.stop("SIGINT");
+            console.log("Bot stopped SIGINT");
+        });
 
         process.once("SIGTERM", () => {
-            bot.stop("SIGTERM")
-            console.log("Bot stopped SIGTERM")
-        })
+            bot.stop("SIGTERM");
+            console.log("Bot stopped SIGTERM");
+        });
     }
-    catch(err) {
-        console.error("Error starting bot: ", err)
-        stopBot()
+    catch (err) {
+        console.error("Error starting bot: ", err);
+        stopBot();
     }
-}
+};
 
 const stopBot = async () => {
-    if(fs.existsSync(lock_file)){
-        fs.unlinkSync(lock_file)
+    if (fs.existsSync(lock_file)) {
+        fs.unlinkSync(lock_file);
     }
 
-    bot.stop("SIGTERM")
-    console.log("bot has been stopped")
-    process.exit(1)
-}
+    bot.stop("SIGTERM");
+    console.log("Bot has been stopped");
+    process.exit(1);
+};
 
-startBot()
+startBot();
