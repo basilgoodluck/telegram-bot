8a0e3696a96daa445c686768090afc764d544059
basilgoodluck <basilgoodluck22@gmail.com>
2024-11-20 21:27:16 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(PORT, () => {
+server.listen(() => {
     console.log(`Server is running at http://${hostname}:${PORT}`);
 });
 
